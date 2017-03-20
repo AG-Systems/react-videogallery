b7a2567991f0195f77ce5df8156489d5ad9a12ac
AG-Systems <area51alienware@gmail.com>
2017-03-20 05:15:25 +0000

    Fixed navbar and added mobile support

--- src/Nav.js
+++ src/Nav.js
@@ -3,43 +3,61 @@ import './Nav.css';
 class Nav extends Component {
   render() {
     return (
-<nav className="navbar navbar-toggleable-md navbar-dark bg-primary">
-        <div className="container">
-          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav1" aria-controls="navbarNav1" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <a className="navbar-brand" href="#">
-            <strong>Navbar</strong>
-          </a>
-          <div className="collapse navbar-collapse" id="navbarNav1">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">Features</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">Pricing</a>
-              </li>
-              <li className="nav-item dropdown btn-group">
-                <a className="nav-link dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                <div className="dropdown-menu dropdown" aria-labelledby="dropdownMenu1">
-                  <a className="dropdown-item">Action</a>
-                  <a className="dropdown-item">Another action</a>
-                  <a className="dropdown-item">Something else here</a>
-                </div>
-              </li>
-            </ul>
-            <form className="form-inline waves-effect waves-light">
-              <input className="form-control" type="text" placeholder="Search" />
-            </form>
-          </div>
-        </div>
-      </nav>
-
+    <nav className="navbar navbar-default">
+            <div className="container-fluid">
+              {/* Brand and toggle get grouped for better mobile display */}
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar" />
+                  <span className="icon-bar" />
+                  <span className="icon-bar" />
+                </button>
+                <a className="navbar-brand" href="#">Brand</a>
+              </div>
+              {/* Collect the nav links, forms, and other content for toggling */}
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                  <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
+                  <li><a href="#">Link</a></li>
+                  <li className="dropdown">
+                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret" /></a>
+                    <ul className="dropdown-menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li role="separator" className="divider" />
+                      <li><a href="#">Separated link</a></li>
+                      <li role="separator" className="divider" />
+                      <li><a href="#">One more separated link</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <form className="navbar-form navbar-left">
+                  <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Search" />
+                  </div>
+                  <button type="submit" className="btn btn-default">Submit</button>
+                </form>
+                <ul className="nav navbar-nav navbar-right">
+                  <li><a href="#">Link</a></li>
+                  <li className="dropdown">
+                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret" /></a>
+                    <ul className="dropdown-menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li role="separator" className="divider" />
+                      <li><a href="#">Separated link</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>{/* /.navbar-collapse */}
+            </div>{/* /.container-fluid */}
+          </nav>
 
     );
   }
 }
+
 export default Nav;
